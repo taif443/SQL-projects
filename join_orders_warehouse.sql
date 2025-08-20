84eef82563c682547d5bba34270c935a99621188
taif443 <t.aif@outlook.com>
2025-08-20 20:26:05 +0300

    Update join_orders_warehouse.sql

--- join_orders_warehouse.sql
+++ join_orders_warehouse.sql
@@ -2,4 +2,4 @@ SELECT *
 FROM my-first-project-445511.warehouse_orders.orders AS orders
 JOIN
 my-first-project-445511.warehouse_orders.warehouse AS warehouse
-ON orders. warehouse_id = warehouse.warehouse_id
+ON orders.warehouse_id = warehouse.warehouse_id
